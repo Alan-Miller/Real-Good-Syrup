48e1258221ca8cf7cc97f48d77ba7d80ec7ab313
Alan-Miller <alan.ashman@gmail.com>
2017-11-22 00:06:09 -0700

    fixing db

--- serverControl.js
+++ serverControl.js
@@ -52,8 +52,8 @@ module.exports = {
   getProducts: function(req, res) {
     // console.log('DB', req.app.get('db'));
     req.app.get('db').get_products(function(err, products) {
-      // console.log('ERR', err);
-      console.log('PRODUCTS', products);
+      console.log('ERR', err);
+      // console.log('PRODUCTS', products);
       res.status(200).json(products);
     });
   },
